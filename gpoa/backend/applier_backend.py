6b65391e0b023faf832d33fc2b6b615ce2a8cd8e
Igor Chudov <nir@altlinux.org>
2019-11-29 18:57:37 +0400

    Try to implement abstract classes

--- gpoa/backend/applier_backend.py
+++ gpoa/backend/applier_backend.py
@@ -1,4 +1,7 @@
-class applier_backend:
+from abc import ABC
+
+class applier_backend(ABC):
+    @classmethod
     def __init__(self):
         pass
 
--- gpoa/frontend/applier_frontend.py
+++ gpoa/frontend/applier_frontend.py
@@ -1,7 +1,11 @@
-class applier_frontend(object):
+from abc import ABC
+
+class applier_frontend(ABC):
+    @classmethod
     def __init__(self, regobj):
         pass
 
+    @classmethod
     def apply(self):
         pass
 
