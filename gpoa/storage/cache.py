40ee4ec8c4d38f4ecc6ad1ffa9244530f0294da9
Igor Chudov <nir@altlinux.org>
2019-12-06 18:27:27 +0400

    Cache storage API stabilized

--- gpoa/storage/cache.py
+++ gpoa/storage/cache.py
@@ -4,3 +4,21 @@ class cache(ABC):
     def __init__(self):
         pass
 
+    @classmethod
+    def store(self, str_id, value):
+        '''
+        '''
+        pass
+
+    @classmethod
+    def get(self, obj_id):
+        '''
+        '''
+        pass
+
+    @classmethod
+    def get_default(self, obj_id, default_value):
+        '''
+        '''
+        pass
+
