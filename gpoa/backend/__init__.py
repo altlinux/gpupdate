41445bf3d4095665acdf0a5814659a46252cbe66
Igor Chudov <nir@altlinux.org>
2019-12-10 18:56:19 +0400

    Implemented factory method for backend

--- gpoa/backend/__init__.py
+++ gpoa/backend/__init__.py
@@ -1,1 +1,8 @@
 from .samba_backend import samba_backend
+
+def backend_factory(loadparm, creds, sid, dc, username):
+    '''
+    Return one of backend objects
+    '''
+    return samba_backend(loadparm, creds, sid, dc, username)
+
