dee217b3dee27c756b61cab88228aac3f0f2e5bf
Igor Chudov <nir@altlinux.org>
2019-12-10 18:55:40 +0400

    Implemented factory methods for storages

--- gpoa/storage/__init__.py
+++ gpoa/storage/__init__.py
@@ -1,3 +1,9 @@
 from .sqlite_registry import sqlite_registry
 from .sqlite_cache import sqlite_cache
 
+def cache_factory(cache_name):
+    return sqlite_cache(cache_name)
+
+def registry_factory(registry_name):
+    return sqlite_registry(registry_name)
+
