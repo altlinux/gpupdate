026b5d7faa35984ea055a8d12cbdced0083955d8
Igor Chudov <nir@altlinux.org>
2019-12-19 23:07:23 +0400

    frontend improvement

--- gpoa/frontend/__init__.py
+++ gpoa/frontend/__init__.py
@@ -10,11 +10,18 @@ import util
 
 import logging
 
+import pwd
+import os
+
 class applier:
-    def __init__(self, username):
+    def __init__(self, username, target):
         self.storage = registry_factory('registry')
         self.username = username
+        self.target = target
         self.sid = util.get_sid(self.storage.get_info('domain'), self.username)
+
+        self.process_uid = os.getuid()
+        self.process_uname = pwd.getpwuid(self.process_uid).pw_name
 
         self.appliers = dict({
             'control':  control_applier(self.storage),
@@ -26,11 +33,20 @@ class applier:
         })
 
     def apply_parameters(self):
-        logging.info('Applying')
-        self.appliers['systemd'].apply()
-        self.appliers['control'].apply()
-        self.appliers['polkit'].apply()
-        self.appliers['firefox'].apply()
-        self.appliers['chromium'].apply()
-        self.appliers['shortcuts'].apply()
+        if 'All' == self.target or 'Computer' == self.target:
+            logging.debug('Applying computer part of settings')
+            if 0 != self.process_uid:
+                logging.error('Not sufficient privileges to run machine appliers')
+                return
+            self.appliers['systemd'].apply()
+            self.appliers['control'].apply()
+            self.appliers['polkit'].apply()
+            self.appliers['firefox'].apply()
+            self.appliers['chromium'].apply()
+            self.appliers['shortcuts'].apply()
+        if self.storage.get_info('machine_sid') != self.sid:
+            if 'All' == self.target or 'User' == self.target:
+                if 0 == self.process_uid:
+                    logging.error('Userspace applier must not be run by root')
+                logging.debug('There are no user settings appliers at the moment')
 
