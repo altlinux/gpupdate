4d3d98a8b43a91aae549a71ad514a2784d4ad1d7
Igor Chudov <nir@altlinux.org>
2019-12-19 20:15:45 +0400

    Shortcut file functionality expanded

--- gpoa/gpt/shortcuts.py
+++ gpoa/gpt/shortcuts.py
@@ -2,6 +2,9 @@ import logging
 
 from xml.etree import ElementTree
 from xdg.DesktopEntry import DesktopEntry
+import json
+
+from util.windows import transform_windows_path
 
 def read_shortcuts(shortcuts_file):
     '''
@@ -13,9 +16,10 @@ def read_shortcuts(shortcuts_file):
 
     for link in xml_root:
         props = link.find('Properties')
-        path = props.get('targetPath')
+        dest = props.get('shortcutPath')
+        path = transform_windows_path(props.get('targetPath'))
         arguments = props.get('arguments')
-        sc = shortcut(path, arguments, link.get('name'))
+        sc = shortcut(dest, path, arguments, link.get('name'))
         sc.set_changed(link.get('changed'))
         sc.set_clsid(link.get('clsid'))
         sc.set_guid(link.get('uid'))
@@ -24,14 +28,25 @@ def read_shortcuts(shortcuts_file):
 
     return shortcuts
 
-def merge_shortcuts(username, sid):
+def json2sc(json_str):
     '''
-    
+    Build shortcut out of string-serialized JSON
     '''
-    return None
+    json_obj = json.loads(json_str)
+
+    sc = shortcut(json_obj['dest'], json_obj['path'], json_obj['arguments'], json_obj['name'])
+    sc.set_changed(json_obj['changed'])
+    sc.set_clsid(json_obj['clsid'])
+    sc.set_guid(json_obj['guid'])
+
+    if 1 == json_obj['is_in_user_context']:
+        sc.set_usercontext(link.get('userContext', False))
+
+    return sc
 
 class shortcut:
-    def __init__(self, path, arguments, name=None):
+    def __init__(self, dest, path, arguments, name=None):
+        self.dest = dest
         self.path = path
         self.arguments = arguments
         self.name = name
@@ -39,11 +54,7 @@ class shortcut:
         self.is_in_user_context = self.set_usercontext()
 
     def __str__(self):
-        result = '<shortcut({} {} {})>'.format(
-            self.path,
-            self.arguments,
-            self.name
-        )
+        result = self.to_json()
         return result
 
     def set_changed(self, change_date):
@@ -64,7 +75,26 @@ class shortcut:
         '''
         self.is_in_user_context = context
 
-    def desktop(self, file_path=None):
+    def to_json(self):
+        '''
+        Return shortcut's JSON for further serialization.
+        '''
+        content = dict()
+        content['dest'] = self.dest
+        content['path'] = self.path
+        content['name'] = self.name
+        content['arguments'] = self.arguments
+        content['clsid'] = self.clsid
+        content['guid'] = self.guid
+        content['changed'] = self.changed
+        content['is_in_user_context'] = self.is_in_user_context
+
+        result = self.desktop()
+        result.content.update(content)
+
+        return json.dumps(result.content)
+
+    def desktop(self):
         '''
         Returns desktop file object which may be written to disk.
         '''
@@ -75,6 +105,9 @@ class shortcut:
         self.desktop_file.set('Terminal', 'false')
         self.desktop_file.set('Exec', '{} {}'.format(self.path, self.arguments))
         self.desktop_file.set('Name', self.name)
-        #desktop_file.write(file_path)
 
         return self.desktop_file
+
+    def write_desktop(self, dest):
+        self.desktop().write(dest)
+
