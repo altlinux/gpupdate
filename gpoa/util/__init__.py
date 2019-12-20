b5f487945d9eafad59575ada62d9f7c0c7c562d4
Igor Chudov <nir@altlinux.org>
2019-12-20 19:24:07 +0400

    Don't import kinit function on top of util module

--- gpoa/util/__init__.py
+++ gpoa/util/__init__.py
@@ -5,10 +5,6 @@ from .util import (
     get_homedir,
     mk_homedir_path
 )
-from .kerberos import (
-    check_krb_ticket,
-    machine_kinit
-)
 from .windows import (
     wbinfo_getsid,
     get_sid,
