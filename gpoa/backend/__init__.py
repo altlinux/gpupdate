ef712fa89630948a669f2a19bda4868c0302df52
Igor Chudov <nir@altlinux.org>
2019-12-20 19:24:42 +0400

    Backend: docstring expanded

--- gpoa/backend/__init__.py
+++ gpoa/backend/__init__.py
@@ -5,7 +5,10 @@ from .samba_backend import samba_backend
 
 def backend_factory(dc, username):
     '''
-    Return one of backend objects
+    Return one of backend objects. Please note that backends must
+    store their configuration in a storage with administrator
+    write permissions in order to prevent users from modifying
+    policies enforced by domain administrators.
     '''
     sc = smbcreds(dc)
     domain = sc.get_domain()
@@ -14,7 +17,7 @@ def backend_factory(dc, username):
     try:
         back = samba_backend(sc, username, domain)
     except Exception as exc:
-        logging.error('Unable to initialize Samba backend')
+        logging.error('Unable to initialize Samba backend: {}'.format(exc))
 
     return back
 
