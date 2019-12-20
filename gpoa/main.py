e52b92e671d5e1d977949d3ecd88ace907aad062
Igor Chudov <nir@altlinux.org>
2019-12-20 19:27:49 +0400

    main.py: Complex runner improvement
    - Option to disable storage update introduced
    - Option to set loglevel introduced
    - Runner refactored into separate short functions
    - Numerous checks for UID introduced

--- gpoa/main.py
+++ gpoa/main.py
@@ -1,27 +1,31 @@
 #! /usr/bin/env python3
 
 import argparse
+import logging
+import os
 
-# Our native control facility
-import util
 from backend import backend_factory
-import frontend
+from frontend.frontend_manager import frontend_manager, determine_username
 from plugin import plugin_manager
 
-# Remove print() from code
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-import pwd
-import os
+from util.util import get_machine_name
+from util.kerberos import machine_kinit
+from util.users import (
+    is_root,
+    get_process_user
+)
+from util.arguments import (
+    set_loglevel,
+    process_target
+)
 
 def parse_arguments():
     arguments = argparse.ArgumentParser(description='Generate configuration out of parsed policies')
     arguments.add_argument('user',
         type=str,
         nargs='?',
-        default=util.get_machine_name(),
-        help='Domain username ({}) to parse policies for'.format(util.get_machine_name()))
+        default=get_machine_name(),
+        help='Domain username ({}) to parse policies for'.format(get_machine_name()))
     arguments.add_argument('--dc',
         type=str,
         help='FQDN of the domain to replicate SYSVOL from')
@@ -34,6 +38,13 @@ def parse_arguments():
     arguments.add_argument('--noupdate',
         action='store_true',
         help='Don\'t try to update storage, only run appliers')
+    arguments.add_argument('--noplugins',
+        action='store_true',
+        help='Don\'t start plugins')
+    arguments.add_argument('--loglevel',
+        type=int,
+        default=0,
+        help='Set logging verbosity level')
     return arguments.parse_args()
 
 class gpoa_controller:
@@ -41,33 +52,60 @@ class gpoa_controller:
     __args = None
 
     def __init__(self):
-        self.__kinit_successful = util.machine_kinit()
+        self.__kinit_successful = machine_kinit()
         self.__args = parse_arguments()
+        set_loglevel(self.__args.loglevel)
 
-        username = self.__args.user
-        target = 'All'
-        if 'Computer' == self.__args.target:
-            target = 'Computer'
-        if 'User' == self.__args.target:
-            target = 'User'
-        logging.debug('Target is: {}'.format(target))
-
-        uname = pwd.getpwuid(os.getuid()).pw_name
+        uname = get_process_user()
         uid = os.getuid()
         logging.debug('The process was started for user {} with UID {}'.format(uname, uid))
-        if not self.__args.noupdate or 0 == uid:
-            back = backend_factory(self.__args.dc, username)
-            if back:
-                back.retrieve_and_store()
 
-        appl = frontend.applier(username, target)
-        appl.apply_parameters()
+        if not is_root():
+            self.username = uname
+        else:
+            self.username = determine_username(self.__args.user)
+        self.target = process_target(self.__args.target)
 
-        pm = plugin_manager()
-        pm.run()
+    def run(self):
+        '''
+        GPOA controller entry point
+        '''
+        self.start_backend()
+        self.start_frontend()
+        self.start_plugins()
+
+    def start_backend(self):
+        '''
+        Function to start update of settings storage
+        '''
+        if not self.__args.noupdate:
+            if is_root():
+                back = backend_factory(self.__args.dc, self.username)
+                if back:
+                    back.retrieve_and_store()
+
+    def start_frontend(self):
+        '''
+        Function to start appliers
+        '''
+        try:
+            appl = frontend_manager(self.username, self.target)
+            appl.apply_parameters()
+        except Exception as exc:
+            logging.error('Error occured while running applier: {}'.format(exc))
+
+    def start_plugins(self):
+        '''
+        Function to start supplementary facilities
+        '''
+        if not self.__args.noplugins:
+            pm = plugin_manager()
+            pm.run()
+
 
 def main():
     controller = gpoa_controller()
+    controller.run()
 
 if __name__ == "__main__":
     main()
