f15aa4f5d4b72b74a6b9e8966db395433cb22335
Igor Chudov <nir@altlinux.org>
2019-12-26 01:00:46 +0400

    plugin.plugin_manager: logging wrapped

--- gpoa/plugin/plugin_manager.py
+++ gpoa/plugin/plugin_manager.py
@@ -2,17 +2,18 @@ import logging
 
 from .adp import adp
 from .exceptions import PluginInitError
+from util.logging import slogm
 
 class plugin_manager:
     def __init__(self):
         self.plugins = dict()
-        logging.info('Starting plugin manager')
+        logging.info(slogm('Starting plugin manager'))
         try:
             self.plugins['adp'] = adp()
-            logging.info('ADP plugin initialized')
+            logging.info(slogm('ADP plugin initialized'))
         except PluginInitError as exc:
             self.plugins['adp'] = None
-            logging.error(exc)
+            logging.error(slogm(exc))
 
     def run(self):
         if self.plugins['adp']:
