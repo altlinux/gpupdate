6b6b2b60f903314c1e49ccba5973e64bffe44ca4
Igor Chudov <nir@altlinux.org>
2019-12-26 01:00:15 +0400

    plugin.adp: logging wrapped

--- gpoa/plugin/adp.py
+++ gpoa/plugin/adp.py
@@ -3,12 +3,13 @@ import subprocess
 
 from util.rpm import is_rpm_installed
 from .exceptions import PluginInitError
+from util.logging import slogm
 
 class adp:
     def __init__(self):
         if not is_rpm_installed('adp'):
             raise PluginInitError('adp is not installed - plugin cannot be initialized')
-        logging.info('ADP plugin initialized')
+        logging.info(slogm('ADP plugin initialized'))
 
     def run(self):
         try:
@@ -16,6 +17,6 @@ class adp:
             subprocess.call(['/usr/bin/adp', 'fetch'])
             subprocess.call(['/usr/bin/adp', 'apply'])
         except Exception as exc:
-            logging.error('Error running ADP')
+            logging.error(slogm('Error running ADP'))
             raise exc
 
