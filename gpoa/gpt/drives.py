0840055450c5290163632d0e89168b63f4808253
Igor Chudov <nir@altlinux.org>
2020-01-10 19:18:39 +0400

    gpt module: Added facilities and stubs to read Printers.xml and other files

---
+++ gpoa/gpt/drives.py
@@ -0,0 +1,78 @@
+#
+# Copyright (C) 2019-2020 Igor Chudov
+# Copyright (C) 2019-2020 Evgeny Sinelnikov
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import json
+from base64 import b64decode
+from Crypto.Cipher import AES
+
+from util_xml import get_xml_root
+
+def read_drives(drives_file):
+    drives = list()
+
+    for drive in get_xml_root(drives_file):
+        drive_obj = drivemap()
+
+        props = drive.find('Properties')
+        drive_obj.set_login(props.get('username'))
+        drive_obj.set_pass(props.get('cpassword'))
+
+        drives.append(drive_obj)
+
+    return drives
+
+def decrypt_pass(cpassword):
+    '''
+    AES key for cpassword decryption: http://msdn.microsoft.com/en-us/library/2c15cbf0-f086-4c74-8b70-1f2fa45dd4be%28v=PROT.13%29#endNote2
+    '''
+    key = (
+            b'\x4e\x99\x06\xe8'
+            b'\xfc\xb6\x6c\xc9'
+            b'\xfa\xf4\x93\x10'
+            b'\x62\x0f\xfe\xe8'
+            b'\xf4\x96\xe8\x06'
+            b'\xcc\x05\x79\x90'
+            b'\x20\x9b\x09\xa4'
+            b'\x33\xb6\x6c\x1b'
+    )
+    cpass_len = len(cpassword)
+    padded_pass = (cpassword + "=" * ((4 - cpass_len % 4) % 4))
+    password = b64decode(padded_pass)
+    decrypter = AES(key, AES.MODE_CBC, '\x00' * 16)
+
+    return decrypter.decrypt(password)
+
+class drivemap:
+    def __init__(self):
+        self.login = None
+        self.password = None
+
+    def set_login(self, username):
+        self.login = username
+
+    def set_pass(self, password):
+        self.password = password
+
+    def to_json(self):
+        drive = dict()
+
+        contents = dict()
+        contents['drive'] = drive
+
+        return json.dumps(contents)
+
---
+++ gpoa/gpt/printers.py
@@ -0,0 +1,122 @@
+#
+# Copyright (C) 2019-2020 Igor Chudov
+# Copyright (C) 2019-2020 Evgeny Sinelnikov
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import json
+
+from util.xml import get_xml_root
+
+def read_printers(printers_file):
+    '''
+    Read printer configurations from Printer.xml
+    '''
+    printers = list()
+
+    for prn in get_xml_root(printers_file):
+        prn_obj = printer(prn.tag, prn.get('name'), prn.get('status'))
+        if 'PortPrinter' == prn.tag:
+            prn_obj.set_ip(prn.get('ipAddress'))
+
+        props = prn.find('Properties')
+        prn_obj.set_location(props.get('location'))
+        prn_obj.set_localname(props.get('localName'))
+        prn_obj.set_comment(props.get('comment'))
+        prn_obj.set_path(props.get('path'))
+
+        printers.append(prn_obj)
+
+    return printers
+
+def json2printer(json_str):
+    '''
+    Build printer object out of string-serialized JSON.
+    '''
+    json_obj = json.loads(json_str)
+
+    prn = printer(json_obj['type'], json_obj['name'], json_obj['status'])
+    prn.set_location(json_obj['location'])
+    prn.set_localname(json_obj['localname'])
+    prn.set_comment(json_obj['comment'])
+    prn.set_path(json_obj['path'])
+    prn.set_ip(json_obj['ip'])
+
+    return prn
+
+class printer:
+    def __init__(self, ptype, name, status):
+        '''
+        ptype may be one of:
+        * LocalPrinter
+        * SharedPrinter
+        * PortPrinter
+        '''
+        self.printer_type = ptype
+        self.name = name
+        self.status = status
+        self.location = None
+        self.localname = None
+        self.comment = None
+        self.path = None
+        self.ip_address = None
+
+    def set_location(self, location):
+        '''
+        Location property usually is a string description of
+        geographical location where printer is residing.
+        '''
+        self.location = location
+
+    def set_localname(self, localname):
+        self.localname = localname
+
+    def set_comment(self, comment):
+        self.comment = comment
+
+    def set_path(self, path):
+        self.path = path
+
+    def set_ip(self, ipaddr):
+        self.ip_address = ipaddr
+
+    def to_json(self):
+        '''
+        Return string-serialized JSON representation of the object.
+        '''
+        printer = dict()
+        printer['type'] = self.printer_type
+        printer['name'] = self.name
+        printer['status'] = self.status
+        printer['location'] = self.location
+        printer['localname'] = self.localname
+        printer['comment'] = self.comment
+        printer['path'] = self.path
+        printer['ip'] = self.ip_address
+
+        # Nesting JSON object into JSON object makes it easier to add
+        # metadata if needed.
+        config = dict()
+        config['printer'] = printer
+
+        return json.dumps(config)
+
+    def cups_config(self):
+        '''
+        Return string-serialized CUPS configuration.
+        '''
+        config = ''
+        return config
+
