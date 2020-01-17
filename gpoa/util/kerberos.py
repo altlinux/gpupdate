07169b412dcca6d113ab8f9d4225d7413fee500d
Viktor Volkov <forexwolf@yandex.ru>
2020-01-17 18:06:02 +0400

    In util/kerberos.py fixes: --------------------------   - Few Lines: expected 2 blank lines   - Line: 38 	pep8: E201 / whitespace after '[' (col 32) 	pep8: E202 / whitespace before ']' (col 46)

--- gpoa/util/kerberos.py
+++ gpoa/util/kerberos.py
@@ -21,6 +21,7 @@ import subprocess
 from .util import get_machine_name
 from .logging import slogm
 
+
 def machine_kinit():
     '''
     Perform kinit with machine credentials
@@ -29,13 +30,14 @@ def machine_kinit():
     subprocess.call(['kinit', '-k', host])
     return check_krb_ticket()
 
+
 def check_krb_ticket():
     '''
     Check if Kerberos 5 ticket present
     '''
     result = False
     try:
-        subprocess.check_call([ 'klist', '-s' ])
+        subprocess.check_call(['klist', '-s'])
         output = subprocess.check_output('klist', stderr=subprocess.STDOUT).decode()
         logging.info(output)
         result = True
