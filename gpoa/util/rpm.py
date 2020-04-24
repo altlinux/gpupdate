b3694a8b4d4f11016922dec78348d797fba153c3
Igor Chudov <nir@altlinux.org>
2020-04-24 15:19:28 +0400

    RPM module improved with corner case checks

--- gpoa/util/rpm.py
+++ gpoa/util/rpm.py
@@ -33,6 +33,60 @@ def is_rpm_installed(rpm_name):
 
     return False
 
+class Package:
+    __install_command = ['/usr/bin/apt-get', '-y', 'install']
+    __remove_command = ['/usr/bin/apt-get', '-y', 'remove']
+
+
+    def __init__(self, package_name):
+        self.package_name = package_name
+        self.for_install = True
+
+        if package_name.endswith('-'):
+            self.package_name = package_name[:-1]
+            self.for_install = False
+
+        self.installed = is_rpm_installed(self.package_name)
+
+    def mark_for_install(self):
+        self.for_install = True
+
+    def mark_for_removal(self):
+        self.for_install = False
+
+    def is_installed(self):
+        return self.installed
+
+    def is_for_install(self):
+        return self.for_install
+
+    def is_for_removal(self):
+        return (not self.for_install)
+
+    def action(self):
+        if self.for_install:
+            if not self.is_installed():
+                return self.install()
+        else:
+            if self.is_installed():
+                return self.remove()
+
+    def install(self):
+        fullcmd = self.__install_command
+        fullcmd.append(self.package_name)
+        return subprocess.check_call(fullcmd)
+
+    def remove(self):
+        fullcmd = self.__remove_command
+        fullcmd.append(self.package_name)
+        return subprocess.check_call(fullcmd)
+
+    def __repr__(self):
+        return self.package_name
+
+    def __str__(self):
+        return self.package_name
+
 
 def update():
     '''
@@ -40,44 +94,64 @@ def update():
     '''
     subprocess.check_call(['/usr/bin/apt-get', 'update'])
 
-
-def install_rpm(rpm_names, force=True):
+def install_rpm(rpm_names, force=True, single_call=True):
     '''
     Install RPM from APT-RPM sources.
 
     :param rpm_names: List of names of RPM packages to install
     :param force: Check if RPM is installed
     '''
-
     install_command = ['/usr/bin/apt-get', '-y', 'install']
 
+    package_list = [Package(rpm_name) for rpm_name in rpm_names]
+
     update()
-    if force:
-        for package in rpm_names:
-            if not is_rpm_installed(package)
-                install_command.append(package)
+
+    filtered_package_list = list()
+    if not force:
+        for package in package_list:
+            if (not package.is_installed()) and package.is_for_install():
+                filtered_package_list.append(package)
+
+            if package.is_installed() and package.is_for_removal():
+                filtered_package_list.append(package)
     else:
-        install_command.extend(rpm_names)
+        filtered_package_list = package_list()
 
-    subprocess.check_call(install_command)
+    if len(filtered_package_list) > 0:
+        if single_call:
+            string_filtered_list = [str(x) for x in filtered_package_list]
+            install_command.extend(string_filtered_list)
+            subprocess.check_call(install_command)
+        else:
+            for package in filtered_package_list:
+                package.action()
 
-def remove_rpm(rpm_names, force=True):
+def remove_rpm(rpm_names, force=True, single_call=True):
     '''
     Remove RPM from file system.
 
     :param rpm_names: List of names of RPM packages to install
-    :param force: Check if rpm is installed
+    :param force: Don't check if rpm is installed
     '''
     remove_command = ['/usr/bin/apt-get', '-y', 'remove']
 
-    update()
+    package_list = [Package(rpm_name) for rpm_name in rpm_names]
 
-    if force:
-        remove_command.extend(rpm_names)
+    filtered_package_list = list()
+    if not force:
+        for package in package_list:
+            if package.is_installed() and package.is_for_removal():
+                filtered_package_list.append(package)
     else:
-        for package in rpm_names:
-            if is_rpm_installed(package):
-                remove_command.append(package)
+        filtered_package_list = package_list
 
-    subprocess.check_call(remove_command)
+    if len(filtered_package_list) > 0:
+        if single_call:
+            string_filtered_list = [str(x) for x in filtered_package_list]
+            remove_command.extend(string_filtered_list)
+            subprocess.check_call(remove_command)
+        else:
+            for package in filtered_package_list:
+                package.action()
 
