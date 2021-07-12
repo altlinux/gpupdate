ee656f52f62120558a484161258e5f97bd99bfb2
Evgeny Sinelnikov <sin@altlinux.org>
2021-07-12 20:16:18 +0400

    Fix user part of GSettings applier

--- gpoa/frontend/appliers/gsettings.py
+++ gpoa/frontend/appliers/gsettings.py
@@ -70,14 +70,14 @@ def glib_value(schema, path, value):
 
 class user_gsetting:
     def __init__(self, schema, path, value, helper_function=None):
-        logging.debug('Creating GSettings element {} (in {}) with value {}'.format(path, schema, value))
+        logging.debug('Creating User GSettings element {} (in {}) with value {}'.format(path, schema, value))
         self.schema = schema
         self.path = path
         self.value = value
         self.helper_function = helper_function
 
     def apply(self):
-        logging.debug('Setting GSettings key {} (in {}) to {}'.format(self.path, self.schema, self.value))
+        logging.debug('Setting User GSettings key {} (in {}) to {}'.format(self.path, self.schema, self.value))
         if self.helper_function:
             self.helper_function(self.schema, self.path, self.value)
         # Get typed value by schema
