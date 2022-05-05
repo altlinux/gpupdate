0e159d34d08adfb79826923f80beba15363648f4
Valery Sinelnikov <greh@altlinux.org>
2022-05-05 16:40:40 +0400

    Added experimental default state for scripts_applier

--- gpoa/frontend/scripts_applier.py
+++ gpoa/frontend/scripts_applier.py
@@ -31,8 +31,8 @@ from .applier_frontend import (
 
 class scripts_applier(applier_frontend):
     __module_name = 'ScriptsApplier'
-    __module_experimental = False
-    __module_enabled = True
+    __module_experimental = True
+    __module_enabled = False
     __cache_scripts = '/var/cache/gpupdate_scripts_cache/machine/'
 
     def __init__(self, storage, sid):
@@ -81,8 +81,8 @@ class scripts_applier(applier_frontend):
 
 class scripts_applier_user(applier_frontend):
     __module_name = 'ScriptsApplierUser'
-    __module_experimental = False
-    __module_enabled = True
+    __module_experimental = True
+    __module_enabled = False
     __cache_scripts = '/var/cache/gpupdate_scripts_cache/users/'
 
     def __init__(self, storage, sid, username):
