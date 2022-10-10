cb6bc1f280af0ce6b9932cfec17fc1b53ae98e5e
Valery Sinelnikov <greh@altlinux.org>
2022-10-10 15:53:11 +0400

    Added use of yandex_applier

--- gpoa/frontend/frontend_manager.py
+++ gpoa/frontend/frontend_manager.py
@@ -66,9 +66,8 @@ from .ini_applier import (
     , ini_applier_user
 )
 
-from .networkshare_applier import (
-      networkshare_applier
-)
+from .networkshare_applier import networkshare_applier
+from .yandex_applier import yandex_applier
 
 from util.sid import get_sid
 from util.users import (
@@ -142,6 +141,7 @@ class frontend_manager:
         self.machine_appliers['systemd'] = systemd_applier(self.storage)
         self.machine_appliers['firefox'] = firefox_applier(self.storage, self.sid, self.username)
         self.machine_appliers['chromium'] = chromium_applier(self.storage, self.sid, self.username)
+        self.machine_appliers['yandex'] = yandex_applier(self.storage, self.sid, self.username)
         self.machine_appliers['shortcuts'] = shortcut_applier(self.storage)
         self.machine_appliers['gsettings'] = gsettings_applier(self.storage, self.file_cache)
         self.machine_appliers['cups'] = cups_applier(self.storage)
