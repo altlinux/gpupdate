54eb4188a78b4b97ed0ba4ce3c2a2ba6e971dd02
Danila Skachedubov <skachedubovda@basealt.ru>
2024-01-16 18:29:49 +0400

    Fixed error message and added logging module

--- gpoa/messages/__init__.py
+++ gpoa/messages/__init__.py
@@ -341,7 +341,7 @@ def warning_code(code):
     warning_ids[19] = 'Failed to change KDE configuration file'
     warning_ids[20] = 'Error connecting to server'
     warning_ids[21] = 'Wallpaper configuration file not found'
-    warning_ids[22] = 'The user setting was not installed, conflict with computer setting.'
+    warning_ids[22] = 'The user setting was not installed, conflict with computer setting'
     warning_ids[23] = 'Action for ini file failed'
 
 
