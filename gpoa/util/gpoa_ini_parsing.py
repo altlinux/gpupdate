caf229a1a432927ed41433e2de77de7a88a8c963
milsnel <danilmirhazov@gmail.com>
2024-11-23 00:07:20 +0500

    Update year range to 2019-2024

--- gpoa/util/gpoa_ini_parsing.py
+++ gpoa/util/gpoa_ini_parsing.py
@@ -1,7 +1,7 @@
 #
 # GPOA - GPO Applier for Linux
 #
-# Copyright (C) 2019-2023 BaseALT Ltd.
+# Copyright (C) 2019-2024 BaseALT Ltd.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
