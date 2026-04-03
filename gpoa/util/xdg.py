389eae26c7413f038585ed5bf24a5bda419e7bfc
Danila Skachedubov <skachedubov@altlinux.org>
2026-04-03 15:36:05 +0400

    Fix user-dirs.dirs generation for non-C locales (Closes: #54590)
    - Add _get_system_lang() helper to detect system locale
    - Generate missing user-dirs.dirs with proper LANG environment
    - Prevent empty DESKTOP path when xdg-user-dirs.dirs missing

--- gpoa/util/xdg.py
+++ gpoa/util/xdg.py
@@ -1,7 +1,7 @@
 #
 # GPOA - GPO Applier for Linux
 #
-# Copyright (C) 2019-2020 BaseALT Ltd.
+# Copyright (C) 2019-2026 BaseALT Ltd.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,14 +16,24 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 import os
+import subprocess
 from functools import lru_cache
 
 from messages import message_with_code
 
 from .logging import log
 from .util import get_homedir
+
+
+def _get_system_lang():
+    try:
+        out = subprocess.check_output(['localectl', 'status'], text=True)
+        for line in out.splitlines():
+            if 'System Locale' in line:
+                return line.split('LANG=', 1)[1].strip()
+    except Exception:
+        return None
 
 
 @lru_cache(maxsize=32)
@@ -35,6 +45,15 @@ def xdg_get_desktop(username, homedir = None):
         logdata = {}
         log('E18', logdata)
         raise Exception(msgtext)
+
+    user_dirs_conf = os.path.join(homedir, '.config', 'user-dirs.dirs')
+    if not os.path.exists(user_dirs_conf):
+        lang = _get_system_lang()
+        if lang and not lang.startswith('C'):
+            os.popen(
+                'export HOME={home}; export LANG={lang}; export LC_ALL={lang}; '
+                'xdg-user-dirs-update'.format(home=homedir, lang=lang)
+            ).read()
 
     stream = os.popen('export HOME={}; xdg-user-dir DESKTOP'.format(homedir))
     output = stream.read()[:-1]
